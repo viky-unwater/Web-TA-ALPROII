8ee95e1b781a959fe53554701518a6f73e26e217
viky-unwater <viky.dwi.thaniya-2022@ftmm.unair.ac.id>
2023-12-28 23:41:13 +0700

    website

--- app.py
+++ app.py
@@ -1,101 +1,119 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify
 import cv2
-import mediapipe as mp
+import string
 import numpy as np
 from cvzone.ClassificationModule import Classifier
-import string
+import mediapipe as mp
+import time
 
-app = Flask(_name_)
-
+app = Flask(__name__)
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.1, max_num_hands=2)
-classifier = Classifier("data/1_Model/keras_model.h5", "data/1_Model/labels.txt")
+classifier = Classifier("keras_model.h5", "labels.txt")
 offset = 20
 imgSize = 500
 labels = list(string.ascii_uppercase)
 
 cap = cv2.VideoCapture(0)
 
-def generate_frames():
+last_prediction = ""  # Variable to store the last prediction
+prediction = ""
+
+@app.route('/get_prediction')
+def get_prediction():
+    global last_prediction
+    return jsonify({'prediction': last_prediction})
+
+def gen_frames():
     while True:
-        ret, frame = cap.read()
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            # Preserve the original BGR color format
+            frame_rgb = frame.copy()
 
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands.process(frame_rgb)
+            all_landmarks = []
 
-        results = hands.process(frame_rgb)
-        all_landmarks = []
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    all_landmarks.extend(hand_landmarks.landmark)
 
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                all_landmarks.extend(hand_landmarks.landmark)
+                    mp_drawing.draw_landmarks(
+                        frame,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
 
-                # Draw landmarks and connections for each hand
-                mp_drawing.draw_landmarks(
-                    frame,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
+                x_min, y_min, x_max, y_max = float('inf'), float('inf'), float('-inf'), float('-inf')
+                for landmark in all_landmarks:
+                    x_min = min(x_min, landmark.x)
+                    y_min = min(y_min, landmark.y)
+                    x_max = max(x_max, landmark.x)
+                    y_max = max(y_max, landmark.y)
 
-            # Extract bounding box for all detected hands
-            x_min, y_min, x_max, y_max = float('inf'), float('inf'), float('-inf'), float('-inf')
-            for landmark in all_landmarks:
-                x_min = min(x_min, landmark.x)
-                y_min = min(y_min, landmark.y)
-                x_max = max(x_max, landmark.x)
-                y_max = max(y_max, landmark.y)
+                x_min, y_min, x_max, y_max = (
+                    max(0, int(x_min * frame.shape[1]) - offset),
+                    max(0, int(y_min * frame.shape[0]) - offset),
+                    min(frame.shape[1], int(x_max * frame.shape[1]) + offset),
+                    min(frame.shape[0], int(y_max * frame.shape[0]) + offset),
+                )
 
-            x_min, y_min, x_max, y_max = (
-                max(0, int(x_min * frame.shape[1]) - offset),
-                max(0, int(y_min * frame.shape[0]) - offset),
-                min(frame.shape[1], int(x_max * frame.shape[1]) + offset),
-                min(frame.shape[0], int(y_max * frame.shape[0]) + offset),
-            )
+                aspect_ratio = (x_max - x_min) / (y_max - y_min)
+                imgWhite = np.ones((imgSize, imgSize, 3), dtype=np.uint8) * 200
 
-            # Calculate aspect ratio of hands image
-            aspect_ratio = (x_max - x_min) / (y_max - y_min)
+                hands_img = frame[y_min:y_max, x_min:x_max]
+                h, w, _ = hands_img.shape
 
-            # Create a white image with the correct data type
-            imgWhite = np.ones((imgSize, imgSize, 3), dtype=np.uint8) * 200
+                if aspect_ratio > 1:
+                    wCal = int(imgSize / aspect_ratio)
+                    imgResize = cv2.resize(hands_img, (wCal, imgSize))
+                    wGap = (imgSize - wCal) // 2
+                    imgWhite[:, wGap:wCal + wGap] = imgResize
+                else:
+                    hCal = int(imgSize * aspect_ratio)
+                    imgResize = cv2.resize(hands_img, (imgSize, hCal))
+                    hGap = (imgSize - hCal) // 2
+                    imgWhite[hGap:hCal + hGap, :] = imgResize
 
-            # Crop the hands_img and resize it to fit inside imgWhite
-            hands_img = frame[y_min:y_max, x_min:x_max]
-            h, w, _ = hands_img.shape
+                prediction, index = classifier.getPrediction(imgWhite)
+                last_prediction = labels[index]
+                cv2.putText(frame, f"Prediction: {labels[index]}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
-            if aspect_ratio > 1:
-                wCal = int(imgSize / aspect_ratio)
-                imgResize = cv2.resize(hands_img, (wCal, imgSize))
-                wGap = (imgSize - wCal) // 2
-                imgWhite[:, wGap:wCal + wGap] = imgResize
-            else:
-                hCal = int(imgSize * aspect_ratio)
-                imgResize = cv2.resize(hands_img, (imgSize, hCal))
-                hGap = (imgSize - hCal) // 2
-                imgWhite[hGap:hCal + hGap, :] = imgResize
-
-            # Perform prediction on imgWhite
-            prediction, index = classifier.getPrediction(imgWhite)
-            print("Prediction for hands:", prediction)
-
-            # Display the prediction on the frame
-            cv2.putText(frame, f"Prediction: {labels[index]}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-
-            # Convert the frame to JPEG
-            ret, jpeg = cv2.imencode('.jpg', frame)
-            frame_bytes = jpeg.tobytes()
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def beranda():
+    return render_template('Beranda.html')
+
+
+@app.route('/interpretasi')
+def interpretasi():
+    return render_template('Interpretasi.html')
+
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if _name_ == "_main_":
+
+@app.route('/tentang')
+def tentang():
+    return render_template('Tentang.html')
+
+
+@app.route('/lainnya')
+def lainnya():
+    return render_template('Lainnya.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,22 +1,102 @@
-from flask import Flask, render_template
+import cv2
+import mediapipe as mp
+import numpy as np
+from cvzone.ClassificationModule import Classifier
+import string
+import time
 
-app = Flask(__name__)
+cap = cv2.VideoCapture(0)
 
-@app.route('/')
-def beranda():
-    return render_template('Beranda.html')
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
-@app.route('/interpretasi')
-def interpretasi():
-    return render_template('Interpretasi.html')
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.1, max_num_hands=2)
+classifier = Classifier("keras_model.h5", "labels.txt")
+offset = 20
+imgSize = 500
+labels = list(string.ascii_uppercase)
 
-@app.route('/tentang')
-def tentang():
-    return render_template('Tentang.html')
+prediction_list = []  # List to store predictions
+start_time = time.time()
 
-@app.route('/lainnya')
-def lainnya():
-    return render_template('Lainnya.html')
+while True:
+    ret, frame = cap.read()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(frame_rgb)
+    all_landmarks = []
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            all_landmarks.extend(hand_landmarks.landmark)
+
+            # Draw landmarks and connections for each hand
+            mp_drawing.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        # Extract bounding box for all detected hands
+        x_min, y_min, x_max, y_max = float('inf'), float('inf'), float('-inf'), float('-inf')
+        for landmark in all_landmarks:
+            x_min = min(x_min, landmark.x)
+            y_min = min(y_min, landmark.y)
+            x_max = max(x_max, landmark.x)
+            y_max = max(y_max, landmark.y)
+
+        x_min, y_min, x_max, y_max = (
+            max(0, int(x_min * frame.shape[1]) - offset),
+            max(0, int(y_min * frame.shape[0]) - offset),
+            min(frame.shape[1], int(x_max * frame.shape[1]) + offset),
+            min(frame.shape[0], int(y_max * frame.shape[0]) + offset),
+        )
+
+        # Calculate aspect ratio of hands image
+        aspect_ratio = (x_max - x_min) / (y_max - y_min)
+
+        # Create a white image with the correct data type
+        imgWhite = np.ones((imgSize, imgSize, 3), dtype=np.uint8) * 200
+
+        # Crop the hands_img and resize it to fit inside imgWhite
+        hands_img = frame[y_min:y_max, x_min:x_max]
+        h, w, _ = hands_img.shape
+
+        if aspect_ratio > 1:
+            wCal = int(imgSize / aspect_ratio)
+            imgResize = cv2.resize(hands_img, (wCal, imgSize))
+            wGap = (imgSize - wCal) // 2
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+        else:
+            hCal = int(imgSize * aspect_ratio)
+            imgResize = cv2.resize(hands_img, (imgSize, hCal))
+            hGap = (imgSize - hCal) // 2
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+
+        # Perform prediction on imgWhite
+        prediction, index = classifier.getPrediction(imgWhite)
+
+        # Add prediction to the list every 3 seconds
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= 3:
+            print("Prediction for hands:", labels[index])
+            prediction_list.append(labels[index])
+            start_time = time.time()
+
+        # Display the prediction on the frame
+        cv2.putText(frame, f"Prediction: {labels[index]}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+    cv2.imshow('frame', frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+# Print the final prediction list
+print("Final Predictions:", prediction_list)
+
+cap.release()
+cv2.destroyAllWindows()
